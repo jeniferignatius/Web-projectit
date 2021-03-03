d2ee5546cfaf36320a5c3673857df5122c22caff
jeniferignatius <41151859+jeniferignatius@users.noreply.github.com>
2021-03-03 13:29:37 +0200

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>news</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="garden1.css">
+    
+</head>
+
+<body>
+    <header class="header">
+        <nav>
+            <a class="logo" href="#"><span>Garden</span></a>
+
+
+            <input class="menu-btn" type="checkbox" id="menu-btn" />
+            <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+
+
+            <ul class="mainmenu">
+                <li><a href="garden1.php"> Etusivu</li>
+                <div class="dropdown">
+            <li class="dropbtn"><a href="#"> Tuotteet <i class="fa fa-caret-down"></i></li>
+            <ul class="dropdown-content">
+              <li><a href="indoor.php">Sisäkasvit </li>
+              <li><a href="#"> Ulkokasvit </li>
+              <li><a href="#">Työkalut</li>
+              <li><a href="#">Kasvien hoito</li>
+              </ul>
+          </div>
+                <li><a href="shops.php"> Myymälät</li>
+                <li class="active"><a href="#">Tietoa meistä</li>
+                <li><a href="contact.php">Ota yhteyttä</li>
+
+                <span class="icon">
+                    <i class="fa fa-fw fa-facebook"></i>
+                    <i class="fa fa-fw fa-instagram"></i>
+                    <i class="fa fa-fw fa-twitter"></i>
+                    <i class="fa fa-fw fa-github"></i>
+                </span>
+            </ul>
+        
+        </nav>
+    </header>
+    <div class="footer">
+        <h3>Information about us</h3><br>
+        <p>
+            Puutarhaliike Neilikka is a store chain founded in 1990, specializing in gardening and houseplants. Our
+            first store was established on Fabianinkatu in Helsinki, and in May 1997 we established the chain's second
+            store in Espoo.
+
+        </p><br><br>
+        <p>
+            Here you will find a comprehensive range of indoor and outdoor plants and, of course, all the tools and
+            other accessories for their care. Our knowledgeable and enthusiastic gardening staff is always ready to help
+            you choose the most suitable products for you.<br><br>
+
+        </p>
+
+    </div>
+    <div class="footer" style="background-color: lightgreen;">
+        <h3 style="color: darkgreen;">@copyright</h3><br>
+        <span class="icon">
+            <i class="fa fa-fw fa-facebook"></i>
+            <i class="fa fa-fw fa-instagram"></i>
+            <i class="fa fa-fw fa-twitter"></i>
+            <i class="fa fa-fw fa-github"></i>
+        </span>
+
+
+    </div>
+</body>
+
+</html>
